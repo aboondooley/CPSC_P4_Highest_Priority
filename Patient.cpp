7e8c338474691f74439d8966ade35eb4c1c6c21a
Alie <aboondooley@gmail.com>
2020-11-03 21:30:06 -0800

    Implemented the Patient class, all working!

--- Patient.cpp
+++ Patient.cpp
@@ -3,3 +3,78 @@
 //
 
 #include "Patient.h"
+#include <string>
+
+using namespace std;
+
+int Patient::numPatients = 0;
+
+Patient::Patient() {
+    name = "";
+    priorityCode = minimal;
+    arrivalOrder = ++numPatients;
+
+}
+
+Patient::Patient(std::string name, Patient::Priority priorityCode) {
+    this->name = name;
+    this->priorityCode = priorityCode;
+    this->arrivalOrder = ++numPatients;
+}
+
+std::string Patient::toString() const {
+    return name + " { pri=" + enumToString(priorityCode) + ", arrive=" +
+           to_string(arrivalOrder) + " }";;
+}
+
+void Patient::setName(std::string newName) {
+    name = newName;
+}
+
+std::string Patient::getName() const {
+    return name;
+}
+
+void Patient::setPriority(Patient::Priority newPriority) {
+    priorityCode = newPriority;
+}
+
+Patient::Priority Patient::getPriority() const {
+    return priorityCode;
+}
+
+int Patient::getArrivalOrder() const {
+    return arrivalOrder;
+}
+
+int Patient::compareTo(Patient &other) const {
+    if (this->priorityCode < other.priorityCode) {
+        return -1;
+    }
+    if (this->priorityCode == other.priorityCode) {
+        if (this->arrivalOrder < other.arrivalOrder) {
+            return -1;
+        }
+    }
+    return 1;
+}
+
+std::string Patient::enumToString(int val) {
+    switch (val) {
+        case 1:
+            return "immediate";
+        case 2:
+            return "emergency";
+        case 3:
+            return "urgent";
+        case 4:
+            return "minimal";
+        default:
+            return "";
+    }
+
+}
+
+
+
+
--- Patient.h
+++ Patient.h
@@ -5,10 +5,33 @@
 #ifndef PROJECT4_HIGHESTPRIORITY_PATIENT_H
 #define PROJECT4_HIGHESTPRIORITY_PATIENT_H
 
+#include <string>
+
+
 
 class Patient {
+public:
+    enum Priority {
+        immediate = 1, emergency = 2, urgent = 3, minimal = 4
+    };
+    Patient();
+    Patient(std::string name, Priority priorityCode);
+    std::string getName() const;
+    void setName(std::string newName);
+    Priority getPriority() const;
+    void setPriority(Priority newPriority);
+    int getArrivalOrder() const;
+    int compareTo(Patient &other) const;
+    std::string toString() const;
+    static std::string enumToString(int val);
 
+private:
+    static int numPatients;
+    std::string name;
+    Priority priorityCode;
+    int arrivalOrder;
 };
+
 
 
 #endif //PROJECT4_HIGHESTPRIORITY_PATIENT_H
