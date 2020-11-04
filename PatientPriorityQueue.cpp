98c3b637ef51cf7b7638c5c909107af5b93e8ac9
Alie <aboondooley@gmail.com>
2020-11-03 22:26:14 -0800

    Implemented enqueue, percolateUp, Size, and the parent, right, left, hasRight, hasLeft. They are not throwing any errors.

--- PatientPriorityQueue.cpp
+++ PatientPriorityQueue.cpp
@@ -1,5 +1,56 @@
 //
-// Created by Alie on 11/1/2020.
+// Created by Alie Boon-Dooley on 11/1/2020.
+// For Seattle University, CPSC 5005, P4.Triage
 //
 
 #include "PatientPriorityQueue.h"
+#include <vector>
+#include <stdexcept>
+using namespace std;
+
+PatientPriorityQueue::PatientPriorityQueue() {}
+
+int PatientPriorityQueue::size() const {
+    return data.size();
+}
+
+void PatientPriorityQueue::enqueue(Patient p) {
+    data.push_back(p);
+    percolateUp(size() - 1);
+}
+
+void PatientPriorityQueue::percolateUp(int index) {
+    if (index > 0) {
+        int p = parent(index);
+        if (data[index].compareTo(data[p]) == -1) {
+            swap(data[index], data[p]);
+            percolateUp(p);
+        }
+    }
+}
+
+int PatientPriorityQueue::parent(int index) const {
+    return (index - 1) / 2;
+}
+
+int PatientPriorityQueue::left(int index) const {
+    return index * 2 + 1;
+}
+
+int PatientPriorityQueue::right(int index) const {
+    return left(index) + 1;
+}
+
+bool PatientPriorityQueue::hasLeft(int parent) const {
+    return left(parent) < size();
+}
+
+bool PatientPriorityQueue::hasRight(int parent) const {
+    return right(parent) < size();
+}
+
+
+
+
+
+
--- PatientPriorityQueue.h
+++ PatientPriorityQueue.h
@@ -1,12 +1,35 @@
 //
-// Created by Alie on 11/1/2020.
+// Created by Alie Boon-Dooley on 11/1/2020.
+// For Seattle University, CPSC 5005, P4.Triage
 //
 
 #ifndef PROJECT4_HIGHESTPRIORITY_PATIENTPRIORITYQUEUE_H
 #define PROJECT4_HIGHESTPRIORITY_PATIENTPRIORITYQUEUE_H
 
+#include <vector>
+#include "Patient.h"
+using namespace std;
 
 class PatientPriorityQueue {
+
+private:
+    std::vector<Patient> data;
+
+    void percolateUp(int index);
+    void percolateDown(int index);
+    void heapify();
+    int parent(int index) const;
+    int left(int index) const;
+    int right(int index) const;
+    bool hasLeft(int parent) const;
+    bool hasRight(int parent) const;
+
+public:
+    PatientPriorityQueue();
+    const Patient &peek() const;
+    Patient dequeue();
+    void enqueue(Patient p);
+    int size() const;
 
 };
 
